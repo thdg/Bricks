fa46e48887c6c5ff120cfcbc3ad6c3857343ad07
thorsteinn <thdg90@gmail.com>
2013-10-12 01:29:43 +0000

    cleanup

--- init.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var g_canvas = document.getElementById("myCanvas");
-var g_ctx = g_canvas.getContext("2d");
-
-g_canvas.height = window.innerHeight;
-g_canvas.width = window.innerWidth;
-
-g_ctx.fillStyle = "white";
